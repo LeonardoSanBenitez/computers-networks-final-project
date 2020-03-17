fbb1bded57db44b1ab0c1d27199f4a3e38d4f90e
Leonardo Benitez <lsbenitezpereira@gmail.com>
2020-03-17 04:56:53 +0000

    correcting bme

--- read_bme280.py
+++ read_bme280.py
@@ -1,12 +1,13 @@
 import bme280
 
-(chip_id, chip_version) = readBME280ID()
-temperature,pressure,humidity = readBME280All()
-print("Chip ID     :" + str(chip_id))
-print("Version     :" + str(chip_version))
+(chip_id, chip_version) = bme280.readBME280ID()
+#print("Chip ID     :" + str(chip_id))
+#print("Version     :" + str(chip_version))
+
 while(1):
-    f = open("log.txt", "w")
-    f.write("Temperature : ", temperature, "C\n")
-    f.write("Pressure: ", pressure, "hPa\n")
-    f.write("Humidity : ", humidity, "%")
-    f.close()
+  temperature,pressure,humidity = bme280.readBME280All()
+  f = open("log.txt", "w")
+  f.write("Temperature : " + str(temperature) + "C\n")
+  f.write("Pressure: " + str(pressure) + "hPa\n")
+  f.write("Humidity : " + str(humidity) + "%")
+  f.close()
