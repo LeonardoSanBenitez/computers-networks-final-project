3bfe867c97b20c45d6d24f559206e7f48c180601
Leonardo Benitez <lsbenitezpereira@gmail.com>
2020-09-04 02:34:37 +0000

    basic mqtt

--- main.py
+++ main.py
@@ -92,6 +92,26 @@ class Motor():
         elif angle<0:
             pass
 
+#TODO: move in __main__
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("redesIFSC/leonardo")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("broker.hivemq.com", 1883, 60)
 
 if __name__ == "__main__":
     explorationRange = 8
@@ -166,6 +186,7 @@ if __name__ == "__main__":
         #              data=json.dumps(data))
         print('sending...')
         print(str(data))#send
+        client.publish('redesIFSC/leonardo', payload='Fora Bolsonaro')
         # Save in SD card
         if flags.exportLog: 
             cv2.imwrite(path_out + 'img' + str(i) + '.jpg', payload3)
