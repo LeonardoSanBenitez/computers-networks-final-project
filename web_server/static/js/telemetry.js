f914d39d85815a59c0963104c677d6520a670b16
Rafael Nagel <rafael.gustavo.nagel@gmail.com>
2020-10-07 21:23:48 -0300

    specify units in front-end

--- web_server/static/js/telemetry.js
+++ web_server/static/js/telemetry.js
@@ -12,13 +12,12 @@ function updateMeasures() {
     .then(json => {
         console.log(json);
         
-        // TODO: specify the measures units 
         document.getElementById('temperature').innerHTML = json.temperature + " ºC";
-        document.getElementById('pressure').innerHTML    = json.pressure + " p";
-        document.getElementById('humidity').innerHTML    = json.humidity;
+        document.getElementById('pressure').innerHTML    = json.pressure + " hPa";
+        document.getElementById('humidity').innerHTML    = json.humidity + " %";
 
     });
 }
 
 updateMeasures();
-setInterval(updateMeasures, 1000 * 60); // in miliseconds
+setInterval(updateMeasures, 1000 * 5); // in miliseconds
